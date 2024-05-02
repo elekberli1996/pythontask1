142dccc4180ffc6f26560858622591180f53cbe5
emin elekberli <elekberli1996@gmail.com>
2024-05-02 18:49:36 +0400

    commit

---
+++ deneme.py
@@ -0,0 +1,45 @@
+#task 1
+# list=[]
+# b=0
+# while b<5:
+#     a=int(input("eded daxil edin"))
+#     b+=1
+#     list.append(a)
+# list.sort()
+# print(list) 
+
+##############################  
+
+#task2
+
+# a= input("bir soz daxil edin")
+# yenilist=[]
+# list=a.split()
+# for i in list:
+#     string=i
+#     sorted_string = ''.join(sorted(string))
+#     yenilist.append(sorted_string) 
+# a=' '.join(yenilist)
+# print(a)
+
+##############################
+
+# a=13
+# b=0
+# while True:
+#     b+=1
+#     c=int(input("cehd edin"))
+#     if a==c:
+#         print(f"tebrikler {b} cehde tapdiniz")
+#         break
+
+############################
+
+# for i in range(2,100):
+#     for j in range(2,i):
+#         if i%j==0 :
+#             break
+#         elif i%j!=0 and i==j+1 :
+#             print(i)
+            
+
